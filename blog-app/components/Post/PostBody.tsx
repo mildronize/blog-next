52f808c6bc311397f11e7e45cc63b8c389507226
Thada Wangthammang <thada.wth@gmail.com>
2022-12-30 19:33:00 +0700

    style: update style

--- blog-app/components/Post/PostBody.tsx
+++ blog-app/components/Post/PostBody.tsx
@@ -6,7 +6,7 @@ interface IPostBodyProps {
 
 export default function PostBody({ content }: IPostBodyProps) {
   return (
-    <div className="max-w-2xl mx-auto prose prose-lg prose-slate dark:prose-dark">
+    <div className="prose prose-lg prose-slate dark:prose-dark">
       <div
         className={markdownStyles['markdown']}
         dangerouslySetInnerHTML={{ __html: content }}
--- blog-app/components/Post/PostHeader.tsx
+++ blog-app/components/Post/PostHeader.tsx
@@ -10,12 +10,12 @@ export default function PostHeader({ title, date }: IPostHeaderProps) {
     <>
       <PostTitle>{title}</PostTitle>
       <div className="hidden md:block md:mb-12">{/* <Avatar name={author.name} picture={author.picture} /> */}</div>
-      <div className="mb-8 md:mb-16 sm:mx-0">
+      <div className="sm:mx-0">
         {/* <CoverImage title={title} src={coverImage} height={620} width={1240} /> */}
       </div>
-      <div className="max-w-2xl mx-auto">
+      <div className="mx-auto -mt-10">
         <div className="block md:hidden mb-6">{/* <Avatar name={author.name} picture={author.picture} /> */}</div>
-        <div className="mb-6 text-lg">
+        <div className="mb-12 sm:text-md text-sm">
           <DateFormatter dateString={date} />
         </div>
       </div>
@@ -25,7 +25,7 @@ export default function PostHeader({ title, date }: IPostHeaderProps) {
 
 function PostTitle({ children }: { children: React.ReactNode }) {
   return (
-    <h1 className="text-3xl lg:text-5xl leading-tight lg:leading-14 mb-12  font-bold tracking-tighter md:tracking-tight text-left">
+    <h1 className="text-2xl lg:text-3xl leading-tight leading-11 lg:leading-12 mb-12 text-left">
       {children}
     </h1>
   );
